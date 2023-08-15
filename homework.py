86d7750953a39dea324ea0505a3c41517f469ada
Vasilii Leontev <leovas1972@gmail.com>
2023-08-15 16:54:12 +0700

    adding the first task

---
+++ homework.py
@@ -0,0 +1,12 @@
+from datetime import time
+
+
+def test_dark_theme_by_time():
+    """
+    Протестируйте правильность переключения темной темы на сайте в зависимости от времени
+    """
+    current_time = time(hour=23)
+    # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
+
+    is_dark_theme = None
+    assert is_dark_theme is True
