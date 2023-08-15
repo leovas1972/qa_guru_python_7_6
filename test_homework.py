67c3a8d00308b1a7c3b2db3ec94886dd9d1c9692
Vasilii Leontev <leovas1972@gmail.com>
2023-08-15 20:12:11 +0700

    adding the second task

--- test_homework.py
+++ test_homework.py
@@ -13,3 +13,19 @@ def test_dark_theme_by_time():
         is_dark_theme = True
 
     assert is_dark_theme is True
+
+def test_dark_theme_by_time_and_user_choice():
+    """
+    Протестируйте правильность переключения темной темы на сайте
+    в зависимости от времени и выбора пользователя
+    dark_theme_enabled_by_user = True - Темная тема включена
+    dark_theme_enabled_by_user = False - Темная тема выключена
+    dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
+    """
+    current_time = time(hour=16)
+    dark_theme_enabled_by_user = True
+    # TODO переключите темную тему в зависимости от времени суток,
+    #  но учтите что темная тема может быть включена вручную
+
+    is_dark_theme = None
+    assert is_dark_theme is True
