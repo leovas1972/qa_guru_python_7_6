d563515af2fdf9cd1f961def1ef3c52ca11767ba
Vasilii Leontev <leovas1972@gmail.com>
2023-08-18 17:37:25 +0700

    third test solution

--- test_homework.py
+++ test_homework.py
@@ -37,3 +37,48 @@ def test_dark_theme_by_time_and_user_choice():
             is_dark_theme = True
 
     assert is_dark_theme is True
+
+def test_find_suitable_user():
+    """
+    Найдите нужного пользователя по условиям в списке пользователей
+    """
+    users = [
+        {"name": "Oleg", "age": 32},
+        {"name": "Sergey", "age": 24},
+        {"name": "Stanislav", "age": 15},
+        {"name": "Olga", "age": 45},
+        {"name": "Maria", "age": 18},
+    ]
+
+    # TODO найдите пользователя с именем "Olga"
+    suitable_users = None
+    def find_list(lists):
+
+        for list in lists:
+            if list['name'] == 'Olga':
+                return list
+
+    suitable_users = find_list(users)
+    assert suitable_users == {"name": "Olga", "age": 45}
+
+    # TODO найдите всех пользователей младше 20 лет
+    suitable_users = []
+
+    for user in users:
+        if user['age'] < 20:
+            suitable_users.append(user)
+
+    assert suitable_users == [
+        {"name": "Stanislav", "age": 15},
+        {"name": "Maria", "age": 18},
+    ]
+
+# Сделайте функцию, которая будет печатать
+# читаемое имя переданной ей функции и значений аргументов.
+# Вызовите ее внутри функций, описанных ниже
+# Подсказка: Имя функции можно получить с помощью func.__name__
+# Например, вызов следующей функции должен преобразовать имя функции
+# в более читаемый вариант (заменить символ подчеркивания на пробел,
+# сделать буквы заглавными (или первую букву), затем вывести значения всех аргументов этой функции:
+# >>> open_browser(browser_name="Chrome")
+# "Open Browser [Chrome]"
