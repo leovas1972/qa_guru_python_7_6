5bcdbe94c0e0e62a2053479cbe496c45373b94e0
Vasilii Leontev <leovas1972@gmail.com>
2023-08-19 10:27:39 +0700

    solution of the fourth task

--- test_homework.py
+++ test_homework.py
@@ -82,3 +82,28 @@ def test_find_suitable_user():
 # сделать буквы заглавными (или первую букву), затем вывести значения всех аргументов этой функции:
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
+
+def test_readable_function():
+    open_browser(browser_name="Chrome")
+    # go_to_companyname_homepage(page_url="https://companyname.com")
+    # find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
+
+
+def open_browser(browser_name):
+    actual_result = write_function_and_arguments(open_browser,browser_name)
+    assert actual_result == "Open Browser [Chrome]"
+
+
+def go_to_companyname_homepage(page_url):
+    actual_result = write_function_and_arguments(go_to_companyname_homepage,page_url)
+    assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    actual_result = write_function_and_arguments(find_registration_button_on_login_page,page_url, button_text)
+    assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+
+def write_function_and_arguments(function, *args):
+    func_name = function.__name__.replace('_', ' ').title()
+    args_name = ", ".join([*args])
+    return f'{func_name} [{args_name}]'
