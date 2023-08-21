18fe02b825720ecf9ef6b8fbef4e16cd7f0d7eaf
Vasilii Leontev <leovas1972@gmail.com>
2023-08-21 18:18:29 +0700

    correction of the fourth task

--- test_homework.py
+++ test_homework.py
@@ -22,19 +22,18 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
-    current_time = time(hour=16)
+    current_time = time(hour=22)
     dark_theme_enabled_by_user = True
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
-    if dark_theme_enabled_by_user:
-        is_dark_theme = True
-    elif not dark_theme_enabled_by_user:
-        is_dark_theme = False
-    elif dark_theme_enabled_by_user is None:
+
+    if dark_theme_enabled_by_user is None:
         if 6 <= current_time.hour <= 22:
             is_dark_theme = False
         else:
             is_dark_theme = True
+    else:
+        is_dark_theme = dark_theme_enabled_by_user
 
     assert is_dark_theme is True
 
